db66fd6c2c8a3b53c60b4482b302d1d3cd890198
dnlolski <vvt56m4k>
2017-07-05 17:55:16 +0200

    2017-07-05

---
+++ IntermediateTutorial/com/OOP/OOP_3.java
@@ -0,0 +1,20 @@
+package com.OOP;
+
+
+public class OOP_3 {
+    public static void main(String[] args) {
+
+        VipCustomer vipAcc = new VipCustomer();
+        System.out.println(vipAcc.getName());
+        System.out.println(vipAcc.getEmail());
+
+        VipCustomer vipAcc1 = new VipCustomer("Daniel",1000);
+        System.out.println(vipAcc1.getName());
+        System.out.println(vipAcc1.getEmail());
+
+        VipCustomer vipAcc2 = new VipCustomer("Robert", 200,"robert@robert.com");
+        System.out.println(vipAcc2.getName());
+        System.out.println(vipAcc2.getEmail());
+
+    }
+}
---
+++ IntermediateTutorial/com/OOP/VipCustomer.java
@@ -0,0 +1,39 @@
+package com.OOP;
+
+
+public class VipCustomer {
+
+    private String name;
+    private double creditLimit;
+    private String email;
+
+    public VipCustomer(){
+        this("Default",1000.00,"default@default.com");
+        System.out.println("Empty constructor called");
+
+    }
+    public VipCustomer(String name, double creditLimit) {
+        this(name, creditLimit, "unknown@unknown.com");
+        System.out.println("second constructor");
+    }
+
+    public VipCustomer(String name, double creditLimit, String email) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.email = email;
+        System.out.println("third constructor");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
+
