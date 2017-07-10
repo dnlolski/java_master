70a2b1ef532edc9b811be4853f6c663ce943a00f
dnlolski <vvt56m4k>
2017-07-10 19:02:28 +0200

    2017-07-05

---
+++ IntermediateTutorial/com/InheritanceChallenge/Main.java
@@ -0,0 +1,14 @@
+package com.InheritanceChallenge;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Porsche porsche = new Porsche(36);
+        porsche.steer(45);
+        porsche.accelerate(20);
+        porsche.accelerate(-10);
+    }
+
+}
---
+++ IntermediateTutorial/com/OOP/Fish.java
@@ -0,0 +1,37 @@
+package com.OOP;
+
+
+public class Fish extends Animal{
+
+    private int gills;
+    private int eyes;
+    private int fins;
+
+    public Fish(String name, int size, int weight, int gills, int eyes, int fins) {
+        super(name, 1, 1, size, weight);
+        this.gills = gills;
+        this.eyes = eyes;
+        this.fins = fins;
+    }
+
+    private void rest(){
+
+    }
+
+    private void moveMuscle(){
+
+    }
+
+    private void moveBackFIn(){
+
+    }
+
+    private void swim(int speed){
+        moveMuscle();
+        moveBackFIn();
+        super.move(speed);
+    }
+
+
+}
+
